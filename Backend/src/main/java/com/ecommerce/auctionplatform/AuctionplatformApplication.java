7c28324436db779bd16150ee85ac0df7f26b41cc
Tinh0804 <lhqtinh2005@gmail.com>
2026-05-30 20:40:24 +0700

    Init:Project

---
+++ Backend/src/main/java/com/ecommerce/auctionplatform/AuctionplatformApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.auctionplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionplatformApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ecommerce/auctionplatform/AuctionplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.auctionplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
