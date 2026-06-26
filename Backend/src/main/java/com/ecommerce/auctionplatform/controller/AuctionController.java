e4b5a4f83ac307d88f8c4d49db42c61609878833
Tinh0804 <lhqtinh2005@gmail.com>
2026-06-26 23:42:28 +0700

    Fix(AuctionController): import UUID to resolve compilation error

--- Backend/src/main/java/com/ecommerce/auctionplatform/controller/AuctionController.java
+++ Backend/src/main/java/com/ecommerce/auctionplatform/controller/AuctionController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/auctions")
